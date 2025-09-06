dd2db809f6cb28e0d4f2b4d0984adf6a72fba841
Raman Verma <ramanverma4183@gmail.com>
2025-09-06 13:54:56 +0530

    update counter_cubit example

--- examples/counter_cubit/bin/counter_cubit.dart
+++ examples/counter_cubit/bin/counter_cubit.dart
@@ -1,1 +1,41 @@
-void main(List<String> arguments) {}
+import 'dart:convert';
+import "dart:io" as io;
+
+import 'package:counter_cubit/counter_cubit.dart';
+
+void main() {
+  final counter = CounterCubit();
+
+  // Draw initial state
+  _renderState(counter.state);
+
+  // Listen for state changes and redraw
+  counter.stream.listen(_renderState);
+
+  // Handle user input
+  io.stdin.lineMode = false;
+  io.stdin
+      .transform(utf8.decoder)
+      .listen(
+        (key) async => switch (key) {
+          'a' || 'A' => counter.increment(),
+          'd' || 'D' => counter.decrement(),
+          'r' || 'R' => counter.reset(),
+          'q' || 'Q' => (() async {
+            await counter.close();
+            io.exit(0);
+          })(),
+          _ => io.stdout.writeln(
+            "Unknown command $key. Use 'a', 'd', 'r', or 'q'.",
+          ),
+        },
+      );
+}
+
+void _renderState(int state) {
+  io.stdout.write('\x1B[2J\x1B[0;0H'); // clear
+  io.stdout.writeln("Counter $state.");
+  io.stdout.writeln(
+    "Press 'a' to increment, 'd' to decrement, 'r' to reset, 'q' to quit.",
+  );
+}
--- examples/counter_cubit/lib/counter_cubit.dart
+++ examples/counter_cubit/lib/counter_cubit.dart
@@ -6,4 +6,8 @@ part 'counter_cubit.g.dart';
 @CubitClass<int>()
 final class CounterCubit extends _$CounterCubit {
   CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
+  void reset() => emit(0);
 }
